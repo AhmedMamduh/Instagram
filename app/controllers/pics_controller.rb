8f3191da63ad078604c7e7492959cde68ab8fda1
Ahmed Mamdouh <ahmed_mu.mamdouh@hotmail.com>
2018-07-12 17:58:30 +0200

    creat new pics actions and view files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,2 +1,26 @@
 class PicsController < ApplicationController
+
+def index
+end
+
+def new
+	@pic = Pic.new
+end
+
+def create
+	@pic = Pic.new(pic_params)
+end
+
+
+private
+
+def pic_params
+	params.require(:pic).permi(:title, :description)
+end
+
+
+
+
+
+
 end
