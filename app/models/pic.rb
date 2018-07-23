33dcfe09c0eb12ecf0a4d48e51056d68230c5e77
Ahmed Mamdouh <ahmed_mu.mamdouh@hotmail.com>
2018-07-23 23:07:43 +0200

    add image

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -2,9 +2,6 @@ class Pic < ApplicationRecord
 	acts_as_votable
 	belongs_to :user
 
-
-  has_attached_file :image
-
   has_attached_file :image, styles: { medium: "300x300>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
